dabcbb69c326eb11b27fb7bd9f45d1d58f47fd0d
DEVCODEFAISHOL <rrqhoshi891@gmail.com>
2025-02-28 07:42:57 +0700

    `feat: updated vite.config.js with new configurations and dependencies`

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,41 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/css/frontend.css', // Tambahkan ini
+                'resources/css/frontend.css',
                 'resources/js/frontend/frontend.js',
             ],
-            refresh: true,
+            refresh: [
+                'resources/views/**',
+                'routes/**',
+                'app/Http/Controllers/**',
+                'app/View/Components/**',
+            ],
         }),
     ],
+    server: {
+        hmr: {
+            overlay: false,
+        },
+        watch: {
+            usePolling: false,
+            interval: 100,
+        },
+    },
+    optimizeDeps: {
+        include: ['axios', 'lodash'],
+    },
+    build: {
+        manifest: true,
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+        minify: process.env.NODE_ENV === 'production',
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['axios', 'lodash'],
+                },
+            },
+        },
+    },
 });
