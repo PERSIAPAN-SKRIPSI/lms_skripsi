4f922628a4a5909ca592b47eb375f14ca5ba733c
DEVCODEFAISHOL <rrqhoshi891@gmail.com>
2025-06-08 18:32:49 +0700

    benerin fitur

--- resources/views/admin/teachers/dashboard.blade.php
+++ resources/views/admin/teachers/dashboard.blade.php
@@ -297,18 +297,7 @@
                                         </svg>
                                         <span>Kelola Kursus</span>
                                     </a>
-                                    <a href="#" class="flex items-center p-2 rounded-lg hover:bg-gray-50 transition">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-blue-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                                        </svg>
-                                        <span>Lihat Siswa</span>
-                                    </a>
-                                    <a href="#" class="flex items-center p-2 rounded-lg hover:bg-gray-50 transition">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-blue-500 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                                        </svg>
-                                        <span>Laporan dan Statistik</span>
-                                    </a>
+
                                 </nav>
                             </div>
                         </div>
